961370beaaf5b96e63e24308f25170eacf347cb2
Paulo Kauscher Pinto <2229378+pkpinto@users.noreply.github.com>
2020-10-27 21:19:50 +0100

    Style with Bootstrap4

--- src/booktracker/app/mongodb.py
+++ src/booktracker/app/mongodb.py
@@ -1,3 +1,4 @@
+from flask import Markup
 from pymongo.mongo_client import MongoClient
 
 
@@ -5,3 +6,63 @@ class Mongodb():
 
     def init_app(self, app):
         self.db = MongoClient('localhost')['booktracker']
+
+    def books(self, filter=None):
+
+        pipeline = [
+            {
+                '$lookup': {
+                    'from' : 'content_metadata',
+                    'localField' : '_id',
+                    'foreignField' : '_id',
+                    'as' : 'content_metadata',
+                }
+            },
+            # {_id:..., ..., content_metadata: [{..., results: [...]}]}
+            {
+                '$replaceRoot': {
+                    'newRoot': {
+                        '$mergeObjects': [ { '$arrayElemAt': [ '$content_metadata', 0 ] }, '$$ROOT' ]
+                    },
+                }
+            },
+            { '$project': { 'content_metadata': False } },
+            # {_id:..., ..., results: {_id: {...}}}
+            {
+                '$replaceRoot': {
+                    'newRoot': {
+                        '$mergeObjects': [ { '$arrayElemAt': [ { '$objectToArray': '$results' }, 0 ] }, '$$ROOT' ]
+                    },
+                }
+            },
+            { '$project': { 'results': False, 'version': False, 'isAuthenticated': False } },
+            {
+                '$replaceRoot': {
+                    'newRoot': {
+                        '$mergeObjects': [ '$v', '$$ROOT' ]
+                    },
+                }
+            },
+            { '$project': { 'k': False, 'v': False } },
+        ]
+
+        # $match should as early as possible in the pipeline
+        if filter is not None:
+            pipeline = [{'$match': filter},] + pipeline
+
+        _books = []
+        for b in self.db['assets'].aggregate(pipeline):
+            _books.append({
+                'assetid': b['_id'],
+                'title': b['name'],
+                'subtitle': b['ebookInfo']['subtitle'],
+                'series': (None if b['ebookInfo'].get('seriesInfo') is None else
+                        ' '.join([b['ebookInfo']['seriesInfo']['seriesName'], b['ebookInfo']['seriesInfo']['sequenceDisplayLabel']])),
+                'author': b['artistName'],
+                'publisher': b['ebookInfo']['publisher'],
+                'date': b['releaseDate'],
+                'ibooks_link': b['ibooks_link'],
+                'description': Markup(b['description']['standard']),
+            })
+
+        return _books
--- src/booktracker/app/routes.py
+++ src/booktracker/app/routes.py
@@ -6,11 +6,29 @@ from flask import render_template, send_file
 from . import mongo
 
 
-@app.route('/assets/<assetid>/artwork')
+@app.route('/', methods=['GET'])
+def home():
+    books = mongo.books()
+    return render_template('index.html', title='home', books=books)
+
+
+@app.route('/books')
+def books():
+    books = mongo.books()
+    return render_template('books.html', title='books', books=books)
+
+
+@app.route('/assets/<assetid>/artwork', methods=['GET'])
 def asset_artwork(assetid):
     art = mongo.db['assets'].find_one({'_id': assetid})['artwork']
     return send_file(io.BytesIO(art), mimetype='image/jpg', as_attachment=False)
 
-@app.route('/assets/<assetid>')
+
+@app.route('/assets/<assetid>', methods=['GET'])
 def asset(assetid):
-    return render_template('asset.html', assetid=assetid)
+    books = mongo.books(filter={'_id': assetid})
+    return render_template('asset.html', title=assetid, books=books)
+
+# @app.route('/assets/<assetid>', methods=['POST'])
+# def asset(assetid):
+#     return render_template('asset.html', title=assetid, assetid=assetid)
